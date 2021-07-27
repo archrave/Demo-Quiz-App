57d3a1f45bc8f55ff06d674de047bb40d33fe225
Divyansh Joshi <divyanshjoshi20@gmail.com>
2021-07-27 16:21:59 +0530

    Created Project

---
+++ lib/screens/quiz_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class QuizScreen extends StatefulWidget {
+  @override
+  _QuizScreenState createState() => _QuizScreenState();
+}
+
+class _QuizScreenState extends State<QuizScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
