665b791e68ac430f6b9b3c1eefd821f6a31e8c7c
Divyansh Joshi <divyanshjoshi20@gmail.com>
2021-07-30 23:15:58 +0530

    Added http package

---
+++ lib/widgets/get_data.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+
+class GetData extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
